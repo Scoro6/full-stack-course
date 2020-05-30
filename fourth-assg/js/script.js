0e0a659bad5d23311f8ddf9bf0f63d7ceb735af0
My name <me@example.com>
2020-05-31 01:36:00 +0530

    Change Made

--- fourth-assg/js/script.js
+++ fourth-assg/js/script.js
@@ -97,14 +97,15 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-     var chosenCategoryShortName = "'"+chooseRandomCategory(categories).short_name+"'";
+     var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+     chosenCategoryShortName="'"+ chosenCategoryShortName +"'";
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
       var homeHtmlToInsertIntoMainPage =
-      insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
+      insertProperty(homeHtml, "randomCategoryShortName" , chosenCategoryShortName);
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -117,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-
+      
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
